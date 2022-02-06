ef8ae523a4f79c83bf9b4307f015de71a36f63b7
chips556 <lohqihern@hotmail.com>
2022-02-06 11:47:07 +0800

    Finished styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+/*global chrome*/
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <div id="buttons">
+        <button className="btn btn-primary" onClick={saveInput}>
+          Save Input
+        </button>
+        <button className="btn btn-success" onClick={saveTab}>
+          Save Tab
+        </button>
+        <button className="btn btn-danger">Delete All</button>
+      </div>
     </div>
   );
 }
